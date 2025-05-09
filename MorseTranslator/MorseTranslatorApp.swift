a0d8d2655ff44fa238f6fcfc9482d5afeb917d8a
bobh <rwhinric@gmail.com>
2025-05-09 11:00:26 -0500

    Initial commit

---
+++ MorseTranslator/MorseTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorseTranslatorApp.swift
+//  MorseTranslator
+//
+//  Created by bobh on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MorseTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
